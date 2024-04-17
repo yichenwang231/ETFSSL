f5c14f3e52eddacb49ea2426f660bb8abb580788
yichenwang231 <124519927+yichenwang231@users.noreply.github.com>
2024-04-17 15:55:53 +0800

    Add files via upload

---
+++ semilearn/core/criterions/__init__.py
@@ -0,0 +1,7 @@
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from .cross_entropy import ce_loss, CELoss
+from .consistency import consistency_loss, ConsistencyLoss
+from .supconloss import SupConLoss
